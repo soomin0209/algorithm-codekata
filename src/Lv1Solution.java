73a0f81fa5ac8246df59891cbb7e75482cb07606
soomin0209 <soomin02094@gmail.com>
2026-01-22 10:15:30 +0900

    클래스 변경

--- src/Lv1Solution.java
+++ src/Lv1Solution.java
@@ -78,46 +78,4 @@ public class Lv1Solution {
         answer = (double)sum / numbers.length;
         return answer;
     }
-
-    // 11. 짝수와 홀수
-    public String solution11(int num) {
-        String answer;
-        if (num % 2 == 0) {
-            answer = "Even";
-        } else {
-            answer = "Odd";
-        }
-        return answer;
-    }
-
-    // 12. 평균 구하기
-    public double solution12(int[] arr) {
-        double sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        double answer = sum / arr.length;
-        return answer;
-    }
-
-    // 13. 자릿수 더하기
-    public int solution13(int n) {
-        int answer = 0;
-        while (n != 0) {
-            answer += n % 10;
-            n /= 10;
-        }
-        return answer;
-    }
-
-    // 14. 약수의 합
-    public int solution14(int n) {
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer += i;
-            }
-        }
-        return answer;
-    }
 }
