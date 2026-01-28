811a44cb0e3f0ca0798311f3e62c4b6bd1209424
soomin0209 <soomin02094@gmail.com>
2026-01-28 10:29:19 +0900

    24. 서울에서 김서방 찾기

--- src/Lv2Solution.java
+++ src/Lv2Solution.java
@@ -188,4 +188,16 @@ public class Lv2Solution {
 
         return count;
     }
+
+    // 24. 서울에서 김서방 찾기
+    public String solution24(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+        return answer;
+    }
 }
