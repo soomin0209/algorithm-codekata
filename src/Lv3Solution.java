771db4c091cac398171607cf3f37d8460e2df104
Soomin <soomin02094@gmail.com>
2026-03-30 09:43:19 +0900

    46. 숫자 문자열과 영단어

--- src/Lv3Solution.java
+++ src/Lv3Solution.java
@@ -86,4 +86,17 @@ public class Lv3Solution {
 
         return answer;
     }
+
+    // 46. 숫자 문자열과 영단어
+    public int solution46(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (s.contains(arr[i])) {
+                s = s.replace(arr[i], String.valueOf(i));
+            }
+        }
+
+        return Integer.parseInt(s);
+    }
 }
